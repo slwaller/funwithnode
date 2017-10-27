6ccfbb6866563a7df23c09e06c822a9655c20824
Sam Waller <samuellwaller@gmail.com>
2017-10-27 11:42:23 -0400

    gonna get sunset time for charleston

--- apiCalls/app.js
+++ apiCalls/app.js
@@ -1,9 +1,19 @@
 const express = require("express")
+const request = require("request")
+
 const app = express()
 
 
 app.get("/", function(req, res){
     res.send("Welcome to the Home Page")
+})
+
+
+//SUNSET IN CHARLESTON
+request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22charleston%2C%20sc%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function(err, res, body){
+    if(!err && res.statusCode == 200){
+        console.log(body)
+    }
 })
 
 
