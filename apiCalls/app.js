2b7e7a9b502d098ba688ed6e76a87fa871f8761f
Sam Waller <samuellwaller@gmail.com>
2017-10-30 14:37:21 -0400

    getting sunset time displayed

--- apiCalls/app.js
+++ apiCalls/app.js
@@ -5,15 +5,14 @@ const app = express()
 
 
 app.get("/", function(req, res){
-    res.send("Welcome to the Home Page")
-})
-
-//SUNSET IN CHARLESTON
-request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22charleston%2C%20sc%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function(err, res, body){
-    if(!err && res.statusCode == 200){
-        const data = JSON.parse(body)
-        console.log(data.query.results.channel.astronomy.sunset)
-    }
+    //SUNSET IN CHARLESTON
+    const url = "https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22charleston%2C%20sc%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+    request(url, function(err, response, body){
+        if(!err && res.statusCode == 200){
+            const data = JSON.parse(body)
+            res.render("sunset.ejs", {data: data})
+        }
+    })
 })
 
 
